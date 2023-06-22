2999490cf168cf431f9e68e32241da95958a4ff3
hahunavth <vuthanhha.2001@gmail.com>
2023-06-22 18:44:47 +0700

    update sampling

--- preprocessing/stratified_sampling.py
+++ preprocessing/stratified_sampling.py
@@ -8,7 +8,7 @@ def stratified_sampling(
     X,
     y=None,
     by=None,
-    keep_ths=2
+    keep_thr=2
 ):
     """
     Stratified sampling
@@ -23,6 +23,8 @@ def stratified_sampling(
         Stratify and labels
     by: str
         Key or list of stratify key
+    keep_thr: str
+        Keep sample if lable is less than or equal to keep threshold
     """
     if y is None:
         if isinstance(by, str):
@@ -33,13 +35,23 @@ def stratified_sampling(
             raise Exception("y and by is not defined!")
 
     count_df = y.value_counts().rename_axis('unique_values').reset_index(name='counts')
-    keep_df = X[y.isin(count_df[count_df['counts']<=keep_ths]['unique_values'])]
-    to_split_X_df = X[y.isin(count_df[count_df['counts']>keep_ths]['unique_values'])]
-    to_split_y_df = y[y.isin(count_df[count_df['counts']>keep_ths]['unique_values'])]
+    keep_df = X[y.isin(count_df[count_df['counts']<=keep_thr]['unique_values'])]
+    to_split_X_df = X[y.isin(count_df[count_df['counts']>keep_thr]['unique_values'])]
+    to_split_y_df = y[y.isin(count_df[count_df['counts']>keep_thr]['unique_values'])]
+
+    selection_rate = selection_rate - len(keep_df) / len(y)
+
+    print("Stratified_sampling")
+    print(f"  Keep: {len(keep_df)} ({len(keep_df) / len(y)})")
+    print(f"  New selection_rate: {selection_rate}")
+
+    if selection_rate <= 0.01:
+        raise Exception(f"New selection rate too small: {selection_rate}")
 
     X_s, _, _, _ = train_test_split(to_split_X_df, to_split_y_df, stratify=to_split_y_df, train_size=selection_rate)
+    X_total = pd.concat([X_s, keep_df])
 
-    return pd.concat([X_s, keep_df])
-# backdoor att
-# adversial att
-# stratified_sampling(selection_rate=0.2, X=train_df, by=['intent', 'entities'])
+    print(f"  Original: \t{len(X)}")
+    print(f"  Total: \t{len(X_total)}")
+
+    return X_total
